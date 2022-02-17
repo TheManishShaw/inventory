d087afd492ad5e7140548cae01664a4d2fadb03d
Manish Shaw <manishshaw725@gmail.com>
2022-02-17 17:04:12 +0530

    Update config_c.php

--- cores/inc/config_c.php
+++ cores/inc/config_c.php
@@ -1,10 +1,39 @@
-<?php 
-$sys_link = "http://localhost/inventory";
-$sys_title = "Trapo Inventory";
+
+ <?php 
+//Database Variables
+$db_name="inventory";
+$db_user="root";
+$db_host="localhost";
+$db_pass="";
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+//System Variables
+$sys_link = "http://localhost/testing/inventory";
 $sys_name = "Inventory";
-
-
-
-
-
- ?>
+$sys_title = "Inventory";
+$sys_fav = "data/sys/logo_mins.png";
+$sys_session = "trapo_session";
+$sys_email = "no-reply@traderg.in";
+$sys_bccemail = "team@traderg.in";
+$sys_dark_logo = "https://manage.traderg.in/assets/images/logo-dark.png";
+$sys_light_logo = "https://manage.traderg.in/assets/images/logo-dark.png";
+$sys_bgimage = "data/sys/bg.jpg";
+$sys_defmsub = $sys_name;
+$sys_nameabbr = "ST.";
+$sys_signup = "enabled";
+$sys_login = "enabled";
+$mob_check = "disabled";
+$email_check = "disabled";
+$sys_phone = "8777807321";
+$sys_smsapi = urlencode('fX3Ht8k51/k-Jp301nq415xrruTKOCEKgUGXzynpuW');
+$sys_smssender = urlencode('GARIND');
+$sys_smsverifyorg = "TraderG";
+date_default_timezone_set("Asia/Kolkata");
+$date = date("d/m/Y");
+$def_utype = "GRP00";
+$def_ustats = "active";
+?>
