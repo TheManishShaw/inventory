18a500077cf79c5d076ff6759730866a2e730b21
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-03-01 06:59:34 +0530

    tooltip problem resolved

--- inventory/setting/tax/modal/create_tax.php
+++ inventory/setting/tax/modal/create_tax.php
@@ -104,4 +104,8 @@
             }
         }
     );
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 </script>
